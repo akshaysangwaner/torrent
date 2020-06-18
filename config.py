52b2eda9bb19f7125e5f8d63335118509da1710d
akshaysangwaner <67052250+akshaysangwaner@users.noreply.github.com>
2020-06-18 17:14:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,30 @@
+import os
 
+class Config(object):
+    # get a token from @BotFather
+    TG_BOT_TOKEN = "1296773508:AAHekUZARWbP9L8TyCXH82ZAaidFRdXulag"
+    # The Telegram API things
+    APP_ID = "1500369"
+    API_HASH = "83f9697774cb30b5a0a2fa24535870f5"
+    # Get these values from my.telegram.org
+    # to store the channel ID who are authorized to use the bot
+    AUTH_CHANNEL = "-1001087181331"
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 1572864000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    #
+    ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", 6800))
+    EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", 1))
+    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600))
+    MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", 1072864000))
